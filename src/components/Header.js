18dadb68129271dec9ee1c90b3c44a01d2d0003b
Shefin <shefinptr@gmail.com>
2025-09-26 23:44:23 +0530

    beauty of components

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+// import { Component } from "react";
+
+// class Header extends Component {
+
+//     render(){
+//         return (<h1>Hello I am {this.props.data} </h1>)
+//     }
+
+
+// }
+
+
+function Header(props) {
+    return(<h1> Hello I am {props.data} </h1>)
+}
+
+
+export default Header;
